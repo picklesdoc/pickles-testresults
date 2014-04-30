db690b6561b95c6bdc5d79913cb56f8c63462742
Dirk Rombauts <dirk.rombauts@questmaster.net>
2014-04-30 16:08:31 +0200

    remove nunit option that don't really matter

--- CreateTestresults.cmd
+++ CreateTestresults.cmd
@@ -5,7 +5,7 @@ ECHO Remember to build the solution first!
 REM pause
 
 Cd "%~dp0\TestHarness\packages\NUnit.Runners.*\tools"
-"nunit-console.exe" "%~dp0\TestHarness\nunit\bin\Debug\nunitHarness.dll" /result="%~dp0\results-example-nunit.xml" /nologo /nodots
+"nunit-console.exe" "%~dp0\TestHarness\nunit\bin\Debug\nunitHarness.dll" /result="%~dp0\results-example-nunit.xml"
 
 
 Cd "%~dp0\TestHarness\packages\SpecRun.Runner.*\tools"
