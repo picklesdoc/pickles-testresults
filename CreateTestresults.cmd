d0a08c675ab765b1668899498851fecb0b8449b0
Dirk Rombauts <dirk.rombauts@questmaster.net>
2014-04-30 15:57:05 +0200

    Add ability to create specrun report

--- CreateTestresults.cmd
+++ CreateTestresults.cmd
@@ -6,6 +6,11 @@ REM pause
 Cd "%~dp0\TestHarness\packages\NUnit.Runners.*\tools"
 "nunit-console.exe" "%~dp0\TestHarness\nunit\bin\Debug\nunitHarness.dll" /result="%~dp0\results-example-nunit.xml" /nologo /nodots
 
+
+Cd "%~dp0\TestHarness\packages\SpecRun.Runner.*\tools"
+"specrun.exe" run default.srprofile "/baseFolder:%~dp0\TestHarness\SpecRun\bin\Debug" /log:specrun.log /report:"%~dp0\results-example-specrun.html"
+
+
 pause
 
 @popd
