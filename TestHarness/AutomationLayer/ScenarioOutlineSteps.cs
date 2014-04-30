6255f3a5f22b208a8d112b9b1369d0523f98fe31
Dirk Rombauts <dirk.rombauts@questmaster.net>
2014-04-30 15:24:38 +0200

    Add scenario outline examples and automation layer

---
+++ TestHarness/AutomationLayer/ScenarioOutlineSteps.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Shouldly;
+
+using TechTalk.SpecFlow;
+
+namespace AutomationLayer
+{
+  [Binding]
+  public class ScenarioOutlineSteps
+  {
+    [Then(@"the scenario will '(.*)'")]
+    public void ThenTheScenarioWill(string result)
+    {
+      switch (result.ToUpperInvariant())
+      {
+        case "PASS":
+          {
+            // nothing to be done
+            break;
+          }
+
+        case "FAIL":
+          {
+            true.ShouldBe(false);
+            break;
+          }
+
+        default:
+          {
+            ScenarioContext.Current.Pending();
+            break;
+          }
+      }
+    }
+  }
+}
