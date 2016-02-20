d86cdfd8f6c098c3e390ece9fc2eae231b13b231
Dirk Rombauts <dirk.rombauts@questmaster.net>
2016-02-20 15:31:56 +0100

    Make the scenario with the non-integer number fail

--- TestHarness/Cucumber/features/step_definitions/AdditionSteps.rb
+++ TestHarness/Cucumber/features/step_definitions/AdditionSteps.rb
@@ -7,6 +7,10 @@ Given(/^I have entered (\d+) into the calculator$/) do |arg1|
   @numbersList << arg1.to_i
 end
 
+Given(/^I have entered (\d+)\.(\d+) into the calculator$/) do |arg1, arg2|
+  expect("this is a hacky way of making the scenario with a non-integer number").to eql("fail")
+end
+
 When(/^I press add$/) do
   @numbersList.each do |number|
       @result = @result + number
@@ -15,4 +19,8 @@ end
 
 Then(/^the result should be (\d+) on the screen$/) do |arg1|
   expect(@result).to eql(arg1.to_i)
+end
+
+Then(/^the result should be (\d+)\.(\d+) on the screen$/) do |arg1, arg2|
+  expect("this is a hacky way of making the scenario with a non-integer number").to eql("fail")
 end
