dea303cf941e3a2c6353b8a98e46d683fe8a39a7
Daniel Pullwitt <daniel.pullwitt@gmail.com>
2016-04-26 00:43:29 +0200

    Adapt to cucumber-js 0.10.x

--- TestHarness/CucumberJS/features/stepdefinitions/MinimalSteps.js
+++ TestHarness/CucumberJS/features/stepdefinitions/MinimalSteps.js
@@ -11,7 +11,7 @@ module.exports = function() {
 
   this.Then(/^inconclusive step$/, function (callback) {
     // we want pending here
-    callback.pending();
+    callback(null, 'pending');
   });
 
   this.Then(/^failing step$/, function (callback) {
